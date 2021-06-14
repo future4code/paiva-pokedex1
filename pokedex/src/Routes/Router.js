194b515baddea7e484a79d377025ddd3d386d23c
dan-ueno <ueno2005@gmail.com>
2021-06-14 18:00:12 -0300

    router atualizado

--- pokedex/src/Routes/Router.js
+++ pokedex/src/Routes/Router.js
@@ -1,33 +1,35 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from 'react-dom'
-import CardDetails from "../Components/CardDetails";
-import CardPoke from "../Components/CardPoke";
-import ErrorPage from "../Pages/Error/ErrorPage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "../Pages/HomePage/HomePage";
+import DetailsPage from "../Pages/DetailsPage/DetailsPage";
+import ErrorPage from "../Pages/Error/ErrorPage";
+import PokedexPage from "../Pages/PokedexPage/PokedexPage";
 
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
 
+        <Route exact path="/page/:page">
+          <HomePage />
+        </Route>
 
-export default function Router() {
-    return (
+        <Route exact path="/pokedex">
+          <PokedexPage />
+        </Route>
 
-<BrowserRouter>
-<Switch>
-    <Route exact path="/">
-        <CardPoke/>
-    </Route>
-    <Route exact path="/page/:page">
-        <CardPoke/>
-    </Route>
-    <Route exact path="/character/:id">
-        <CardDetails/>
-    </Route>
-    <Route>
-        <HomePage/>
-    </Route>
-    <Route>
-        <ErrorPage/>
-    </Route>
-</Switch>
-</BrowserRouter>
-    )
+        <Route exact path="/pokemon/:id">
+          <DetailsPage />
+        </Route>
+
+        <Route>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
+export default Router
