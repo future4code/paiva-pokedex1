16c6d1cc090c2f35cb2ecf9a4528a7987e3d86a4
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-06-19 22:40:45 -0300

    correção da responsividade do cardDetails

--- pokedex/src/Components/CardDetails/Styled.js
+++ pokedex/src/Components/CardDetails/Styled.js
@@ -31,9 +31,8 @@ export const ContainerDetalhes = styled.div`
 `
 
 export const Container = styled.div` 
-    /* height: 100vh; */
     display: flex;
     justify-content: center;
     align-items: center; 
-    margin-top: 40px;
+    margin-top: 50px;
 `
